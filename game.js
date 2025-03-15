9bdd705e35d1bd9d2984aa45fde8121ee84a7131
JamiSoini <158048822+JamiSoini@users.noreply.github.com>
2025-03-15 21:07:19 +0200

    Add a player choise function and score variables

--- game.js
+++ game.js
@@ -2,12 +2,30 @@
 function getComputerChoise() {
     let choise = Math.round(Math.random() * 2);
     if (choise === 0) {
-        return console.log("Rock!");
+        return "Rock!";
     } else if (choise === 1) {
-        return console.log("Paper!");
+        return "Paper!";
     } else {
-        return console.log("Scissors!");
+        return "Scissors!";
     }
 }
 
-getComputerChoise();
+function getHumanChoise() {
+    let choise = prompt("Choose! rock, paper or scissors:");
+    if (choise === "rock") {
+        return "Rock!";
+    } else if (choise === "paper") {
+        return "Paper!";
+    } else {
+        return "Scissors!";
+    }
+}
+
+let humanScore = 0;
+let computerScore = 0;
+
+console.log(getComputerChoise());
+console.log(getHumanChoise());
+
+console.log(computerScore);
+console.log(humanScore);
