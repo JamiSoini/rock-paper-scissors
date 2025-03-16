7f766dac9d34432dbd0e6c2d6a97e60b961373aa
JamiSoini <158048822+JamiSoini@users.noreply.github.com>
2025-03-16 05:14:08 +0200

    Make the game logic needed to play a round
    Also made the user input to disregard uppercase
    letters.

--- game.js
+++ game.js
@@ -2,30 +2,54 @@
 function getComputerChoise() {
     let choise = Math.round(Math.random() * 2);
     if (choise === 0) {
-        return "Rock!";
+        return "rock";
     } else if (choise === 1) {
-        return "Paper!";
+        return "paper";
     } else {
-        return "Scissors!";
+        return "scissors";
     }
 }
 
 function getHumanChoise() {
     let choise = prompt("Choose! rock, paper or scissors:");
+    choise = choise.toLowerCase();
     if (choise === "rock") {
-        return "Rock!";
+        return "rock";
     } else if (choise === "paper") {
-        return "Paper!";
+        return "paper";
     } else {
-        return "Scissors!";
+        return "scissors";
     }
 }
+
+function playRound(humanChoise, computerChoise) {
+    if (humanChoise === "paper" && computerChoise === "rock") {
+        return console.log("You win!");
+    } else if (humanChoise === "paper" && computerChoise === "scissors") {
+        return console.log("You lose! ;(");
+    } else if (humanChoise === "rock" && computerChoise === "scissors") {
+        return console.log("You win!");
+    } else if (humanChoise === "rock" && computerChoise === "paper") {
+        return console.log("You lose! ;(");
+    } else if (humanChoise === "scissors" && computerChoise === "paper") {
+        return console.log("You win!");
+    } else if (humanChoise === "scissors" && computerChoise === "rock") {
+        return console.log("You lose! ;(");
+    } else {
+        return console.log("It's a draw!");
+    }
+}
+
+const humanSelection = getHumanChoise();
+const computerSelection = getComputerChoise();
+
+playRound(humanSelection, computerSelection);
+
+console.log(computerSelection);
+console.log(humanSelection);
 
 let humanScore = 0;
 let computerScore = 0;
 
-console.log(getComputerChoise());
-console.log(getHumanChoise());
-
-console.log(computerScore);
-console.log(humanScore);
+console.log(`Computer's score:${computerScore}`);
+console.log(`Your score:${humanScore}`);
